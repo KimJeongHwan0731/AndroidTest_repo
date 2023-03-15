14e48d48bc4454fd7054458799ce4c7fc6db5eab
Kim Jeonghwan <jeonghwankim0731@gmail.com>
2023-03-15 16:39:38 +0900

    [update] second work

--- app/src/main/java/com/example/androidfirst/MainActivity2.kt
+++ app/src/main/java/com/example/androidfirst/MainActivity2.kt
@@ -1,10 +1,9 @@
-package com.example.androidgithubtest
+package com.example.androidfirst
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.androidfirst.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub3)
