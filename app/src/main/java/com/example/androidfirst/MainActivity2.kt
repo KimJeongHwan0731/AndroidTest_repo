ce7ceeb9a5189f908fc315072b13cdde5a7aa6e3
Kim Jeonghwan <jeonghwankim0731@gmail.com>
2023-03-15 17:29:09 +0900

    [update] third work

--- app/src/main/java/com/example/androidfirst/MainActivity2.kt
+++ app/src/main/java/com/example/androidfirst/MainActivity2.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sub3)
+        setContentView(R.layout.activity_sub4)
     }
 }
--- app/src/main/java/com/example/androidgithubtest/MainActivity.kt
+++ app/src/main/java/com/example/androidgithubtest/MainActivity.kt
@@ -7,6 +7,6 @@ import com.example.androidfirst.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sub3)
+        setContentView(R.layout.activity_sub4)
     }
 }
