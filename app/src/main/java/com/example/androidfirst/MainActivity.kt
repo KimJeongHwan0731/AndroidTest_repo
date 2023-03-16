89a9c58264af515558915f9e328ace3857f6b609
Kim Jeonghwan <jeonghwankim0731@gmail.com>
2023-03-16 17:36:28 +0900

    [update] fourth work

--- app/src/main/java/com/example/androidfirst/MainActivity.kt
+++ app/src/main/java/com/example/androidfirst/MainActivity.kt
@@ -1,29 +1,64 @@
 package com.example.androidfirst
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.InputType
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.androidfirst.databinding.ActivitySub2Binding
+import com.example.androidfirst.databinding.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivitySub2Binding
-    var flag: Boolean = false;
+
+    lateinit var binding: ActivitySub4Binding
+    lateinit var binding2: ActivitySub11Binding
+    lateinit var binding3: ActivitySub13Binding
+    lateinit var binding4: ActivitySub15Binding
+    lateinit var binding5: ActivitySub16Binding
+    var flag: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySub2Binding.inflate(layoutInflater)
+        binding = ActivitySub4Binding.inflate(layoutInflater)
+        binding2 = ActivitySub11Binding.inflate(layoutInflater)
+        binding3 = ActivitySub13Binding.inflate(layoutInflater)
+        binding4 = ActivitySub15Binding.inflate(layoutInflater)
+        binding5 = ActivitySub16Binding.inflate(layoutInflater)
+        setContentView(binding5.root)
 
-        setContentView(binding.root)
+//        binding3.btnClick.setOnClickListener {
+//            if(flag == true) {
+//                binding3.btnTarget.visibility = View.VISIBLE
+//                binding3.btnGone.visibility = View.VISIBLE
+//                flag = false
+//            } else {
+//                binding3.btnTarget.visibility = View.INVISIBLE
+//                binding3.btnGone.visibility = View.GONE
+//                flag = true
+//            }
+//
+//        binding4.button13.setOnClickListener {
+//            if(flag == true) {
+//                binding4.checkBox.isChecked = true
+//                binding4.checkBox2.isChecked = true
+//                flag = false
+//            } else {
+//                binding4.checkBox.isChecked = false
+//                binding4.checkBox2.isChecked = false
+//                flag = true
+//            }
+//        }
 
-        binding.btnClick.setOnClickListener {
-            Toast.makeText(applicationContext, "김정환님 반갑습니다", Toast.LENGTH_SHORT).show()
-            if (flag == true) {
-                binding.tvClassName.text = "안드로이드웹앱3기"
-                flag = false
-            } else {
-                binding.tvClassName.text = "안웹앱3기"
-                flag = true
-            }
+        binding5.edtPassword.setOnLongClickListener {
+            binding5.edtPassword.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+            Toast.makeText(applicationContext,binding5.edtPassword.text.toString(),Toast.LENGTH_SHORT).show()
+            Handler(Looper.getMainLooper()).postDelayed({
+            }, 2000)
+            binding5.edtPassword.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+            return@setOnLongClickListener(true)
         }
+//        binding5.edtPassword.setOnClickListener{
+//            binding5.edtPassword.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+//        }
     }
 }
