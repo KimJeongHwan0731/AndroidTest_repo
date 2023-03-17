eb1569363365b028bc97dfb5e42578ee23b2c4aa
Kim Jeonghwan <jeonghwankim0731@gmail.com>
2023-03-17 17:39:03 +0900

    [update] 5 word

--- app/src/main/java/com/example/androidfirst/MainActivity.kt
+++ app/src/main/java/com/example/androidfirst/MainActivity.kt
@@ -1,64 +1,42 @@
 package com.example.androidfirst
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.text.InputType
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.androidfirst.databinding.*
+import com.example.androidfirst.databinding.ActivitySub23Binding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivitySub4Binding
-    lateinit var binding2: ActivitySub11Binding
-    lateinit var binding3: ActivitySub13Binding
-    lateinit var binding4: ActivitySub15Binding
-    lateinit var binding5: ActivitySub16Binding
+    lateinit var binding: ActivitySub23Binding
     var flag: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySub4Binding.inflate(layoutInflater)
-        binding2 = ActivitySub11Binding.inflate(layoutInflater)
-        binding3 = ActivitySub13Binding.inflate(layoutInflater)
-        binding4 = ActivitySub15Binding.inflate(layoutInflater)
-        binding5 = ActivitySub16Binding.inflate(layoutInflater)
-        setContentView(binding5.root)
+        binding = ActivitySub23Binding.inflate(layoutInflater)
 
-//        binding3.btnClick.setOnClickListener {
-//            if(flag == true) {
-//                binding3.btnTarget.visibility = View.VISIBLE
-//                binding3.btnGone.visibility = View.VISIBLE
-//                flag = false
-//            } else {
-//                binding3.btnTarget.visibility = View.INVISIBLE
-//                binding3.btnGone.visibility = View.GONE
-//                flag = true
-//            }
-//
-//        binding4.button13.setOnClickListener {
-//            if(flag == true) {
-//                binding4.checkBox.isChecked = true
-//                binding4.checkBox2.isChecked = true
-//                flag = false
-//            } else {
-//                binding4.checkBox.isChecked = false
-//                binding4.checkBox2.isChecked = false
-//                flag = true
-//            }
-//        }
-
-        binding5.edtPassword.setOnLongClickListener {
-            binding5.edtPassword.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
-            Toast.makeText(applicationContext,binding5.edtPassword.text.toString(),Toast.LENGTH_SHORT).show()
-            Handler(Looper.getMainLooper()).postDelayed({
-            }, 2000)
-            binding5.edtPassword.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
-            return@setOnLongClickListener(true)
+        setContentView(binding.root)
+        //버튼1 선택 시, 화면1 노출 및 버튼2 선택 시, 화면 2 노출 로직
+        binding.btnSelectOne.setOnClickListener {
+            if (flag == true) {
+                binding.screenOne.visibility = View.VISIBLE
+                binding.screenTwo.visibility = View.INVISIBLE
+                flag = false
+            } else {
+                binding.screenOne.visibility = View.INVISIBLE
+                binding.screenTwo.visibility = View.VISIBLE
+                flag = true
+            }
         }
-//        binding5.edtPassword.setOnClickListener{
-//            binding5.edtPassword.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
-//        }
+
+        //프레임레이아웃 -> 리니어레이아웃 1번화면
+        binding.imgPictuerOne.setOnClickListener {
+            Toast.makeText(this, "1번화면입니다.", Toast.LENGTH_SHORT).show()
+        }
+        //프레임레이아웃 -> 리니어레이아웃 2번화면
+        binding.imgPictuerTwo.setOnClickListener {
+            Toast.makeText(this, "2번화면입니다.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
