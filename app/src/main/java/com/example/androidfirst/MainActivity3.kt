ff239cabd22560139c105292fef860a1c7a91313
Kim Jeonghwan <jeonghwankim0731@gmail.com>
2023-03-21 15:29:52 +0900

    [update] 6 work

--- app/src/main/java/com/example/androidfirst/MainActivity3.kt
+++ app/src/main/java/com/example/androidfirst/MainActivity3.kt
@@ -1,32 +1,42 @@
 package com.example.androidfirst
 
 import android.os.Bundle
-import android.util.Log
-import android.view.MotionEvent
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.androidfirst.databinding.ActivityMain4Binding
+import com.example.androidfirst.databinding.ActivityMain3Binding
 
-class MainActivity3 : AppCompatActivity() {
-    lateinit var binding : ActivityMain4Binding
+class MainActivity3 : AppCompatActivity(), View.OnClickListener {
+    lateinit var binding: ActivityMain3Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMain4Binding.inflate(layoutInflater)
+        binding = ActivityMain3Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnClick1.setOnClickListener(this)
+        binding.btnClick2.setOnClickListener(this)
+
+        binding.btnClick3.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(applicationContext, "버튼3번", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        binding.btnClick4.setOnClickListener { v ->
+            Toast.makeText(applicationContext, "버튼4번", Toast.LENGTH_SHORT).show()
+        }
     }
 
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        when(event?.action) {
-            MotionEvent.ACTION_DOWN->{
-                Toast.makeText(applicationContext,"터치를 클릭했습니다", Toast.LENGTH_SHORT).show()
-                Log.d("MainActivity2", "터치클릭 x= ${event?.x} rawx= ${event?.rawX}")
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnClick1 -> {
+                Toast.makeText(applicationContext, "버튼1번", Toast.LENGTH_SHORT).show()
             }
-            MotionEvent.ACTION_UP -> {
-                Toast.makeText(applicationContext,"터치를 클릭해제했습니다", Toast.LENGTH_SHORT).show()
-                Log.d("MainActivity2", "터치를 클릭해제했습니다.")
+            R.id.btnClick2 -> {
+                Toast.makeText(applicationContext, "버튼2번", Toast.LENGTH_SHORT).show()
             }
+            else -> null
         }
-        return super.onTouchEvent(event)
     }
 }
